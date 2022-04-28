df7095a447ed309f3a907e2edf8cb6f8a5ba6767
LaKostyaLi <lakostyali1996@gmail.com>
2022-04-28 23:13:40 +0300

    ♥

---
+++ Assets/LevelManager.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelManager : MonoBehaviour
+{
+    [SerializeField] private GameObject _levelPrefab;
+
+    private void OnButtonClick()
+    {
+        Instantiate(_levelPrefab, new Vector3(0, 0, 0), Quaternion.identity);
+    }
+}
