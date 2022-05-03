279222346fba3833244de3fc794f9d552ab671d6
Maksim Buzuy <mbuzuy@gmail.com>
2022-05-04 01:50:19 +0300

    Menu Added

---
+++ CarBasketBall/Assets/Scripts/Buttons.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Buttons : MonoBehaviour
+{
+    [SerializeField] private GameObject _mainMenu;
+    [SerializeField] private GameObject _inGameMenu;
+
+    public void Awake()
+    {
+        _mainMenu.SetActive(true);
+    }
+    public void OnClickStart()
+    {
+        _mainMenu.SetActive(false);
+        _inGameMenu.SetActive(true);
+    }
+
+    public void OnClickMenu()
+    {
+        _mainMenu.SetActive(true);
+        _inGameMenu.SetActive(false);
+    }
+    public void OnClickExit()
+    {
+        Application.Quit();
+    }
+}
