9c75db066db97425b49415622caeb5f2405ce733
LakostyaLi <lakostyali1996@gmail.com>
2022-05-03 05:33:56 +0300

    Analititcs

---
+++ Assets/Scripts/Analitics.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Analytics;
+
+public class Analitics : MonoBehaviour
+{
+   //на каком уровне чаще всего проигрывает игрок
+   public void OnPlayerDead(int _levelNumber, string player)
+    {
+        Analytics.CustomEvent("Player Dead", new Dictionary<string, object>()
+        {
+            {"Level", _levelNumber},
+            {"Player", player}
+        }
+        );
+    }
+}
